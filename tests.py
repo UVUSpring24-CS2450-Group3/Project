d515ab091ea76cb2ea1462a5a87cc6a8ee47b54a
Braxton Anderson <braxtonaan@gmail.com>
2024-02-10 16:57:10 -0700

    add branch test cases

--- tests.py
+++ tests.py
@@ -2,130 +2,130 @@ from uvsim import UVSim
 
 
 def get_input():
-	return ""
+    return ""
 
 
 
 def test_add(monkeypatch):
-	sim = UVSim()
-	"""
-	RDKB #98
-	RDKB #99
-	ADD #98
-	ADD #99
-	HALT
-	"""
-	sim.loadProgram([1098, 1099, 3098, 3099, 4300])
+    sim = UVSim()
+    """
+    RDKB #98
+    RDKB #99
+    ADD #98
+    ADD #99
+    HALT
+    """
+    sim.loadProgram([1098, 1099, 3098, 3099, 4300])
 
-	monkeypatch.setattr('builtins.input', lambda _: "8")
-	sim.step()
-	monkeypatch.setattr('builtins.input', lambda _: "9")
-	sim.step()
-	sim.step()
-	sim.step()
-	sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "8")
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "9")
+    sim.step()
+    sim.step()
+    sim.step()
+    sim.step()
 
-	assert sim.acc == 17
+    assert sim.acc == 17
 
 def test_sub(monkeypatch):
-	sim = UVSim()
-	"""
-	RDKB #98
-	RDKB #99
-	ADD #98
-	SUB #99
-	HALT
-	"""
-	sim.loadProgram([1098, 1099, 3098, 3199, 4300])
+    sim = UVSim()
+    """
+    RDKB #98
+    RDKB #99
+    ADD #98
+    SUB #99
+    HALT
+    """
+    sim.loadProgram([1098, 1099, 3098, 3199, 4300])
 
-	monkeypatch.setattr('builtins.input', lambda _: "2003")
-	sim.step()
-	monkeypatch.setattr('builtins.input', lambda _: "-208")
-	sim.step()
-	sim.step()
-	sim.step()
-	sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "2003")
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "-208")
+    sim.step()
+    sim.step()
+    sim.step()
+    sim.step()
 
-	assert sim.acc == 2211
+    assert sim.acc == 2211
 
-	sim.reset()
-	"""
-	RDKB #98
-	RDKB #99
-	ADD #98
-	SUB #99
-	HALT
-	"""
-	sim.loadProgram([1098, 1099, 3098, 3199, 4300])
+    sim.reset()
+    """
+    RDKB #98
+    RDKB #99
+    ADD #98
+    SUB #99
+    HALT
+    """
+    sim.loadProgram([1098, 1099, 3098, 3199, 4300])
 
-	monkeypatch.setattr('builtins.input', lambda _: "9999")
-	sim.step()
-	monkeypatch.setattr('builtins.input', lambda _: "8381")
-	sim.step()
-	sim.step()
-	sim.step()
-	sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "9999")
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "8381")
+    sim.step()
+    sim.step()
+    sim.step()
+    sim.step()
 
-	assert sim.acc == 1618
+    assert sim.acc == 1618
 
 def test_divide(monkeypatch):
-	sim = UVSim()
-	sim.loadProgram([1098, 1099, 2098, 3299, 4300])
-	monkeypatch.setattr('builtins.input', lambda _: "155")
-	sim.step()
-	monkeypatch.setattr('builtins.input', lambda _: "5")
-	sim.step()
-	sim.step()
-	sim.step()
-	sim.step()
-	assert sim.acc == 31
+    sim = UVSim()
+    sim.loadProgram([1098, 1099, 2098, 3299, 4300])
+    monkeypatch.setattr('builtins.input', lambda _: "155")
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "5")
+    sim.step()
+    sim.step()
+    sim.step()
+    sim.step()
+    assert sim.acc == 31
 
-	sim.reset()
-	sim.loadProgram([1098, 1099, 2098, 3299, 4300])
-	monkeypatch.setattr('builtins.input', lambda _: "300")
-	sim.step()
-	monkeypatch.setattr('builtins.input', lambda _: "-5")
-	sim.step()
-	sim.step()
-	sim.step()
-	sim.step()
-	assert sim.acc == -60
+    sim.reset()
+    sim.loadProgram([1098, 1099, 2098, 3299, 4300])
+    monkeypatch.setattr('builtins.input', lambda _: "300")
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "-5")
+    sim.step()
+    sim.step()
+    sim.step()
+    sim.step()
+    assert sim.acc == -60
  
 def test_multi(monkeypatch):
-	sim = UVSim()
-	sim.loadProgram([1098, 1099, 2099, 3398, 4300])
-	monkeypatch.setattr('builtins.input', lambda _: "12")
-	sim.step()
-	monkeypatch.setattr('builtins.input', lambda _: "13")
-	sim.step()
-	sim.step()
-	sim.step()
-	sim.step()
-	assert sim.acc == 156
+    sim = UVSim()
+    sim.loadProgram([1098, 1099, 2099, 3398, 4300])
+    monkeypatch.setattr('builtins.input', lambda _: "12")
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "13")
+    sim.step()
+    sim.step()
+    sim.step()
+    sim.step()
+    assert sim.acc == 156
 
-	sim.reset()
-	sim.loadProgram([1098, 1099, 2098, 3399, 4300])
-	monkeypatch.setattr('builtins.input', lambda _: "12")
-	sim.step()
-	monkeypatch.setattr('builtins.input', lambda _: "-13")
-	sim.step()
-	sim.step()
-	sim.step()
-	sim.step()
-	assert sim.acc == -156
+    sim.reset()
+    sim.loadProgram([1098, 1099, 2098, 3399, 4300])
+    monkeypatch.setattr('builtins.input', lambda _: "12")
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "-13")
+    sim.step()
+    sim.step()
+    sim.step()
+    sim.step()
+    assert sim.acc == -156
 
 def test_load(monkeypatch):
-	sim = UVSim()
-	sim.loadProgram([1098, 1099, 2098, 2099, 4300])
-	monkeypatch.setattr('builtins.input', lambda _: "120")
-	sim.step()
-	monkeypatch.setattr('builtins.input', lambda _: "130")
-	sim.step()
-	sim.step()
-	assert sim.acc == 120
-	sim.step()
-	assert sim.acc == 130 
-	sim.step()
+    sim = UVSim()
+    sim.loadProgram([1098, 1099, 2098, 2099, 4300])
+    monkeypatch.setattr('builtins.input', lambda _: "120")
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "130")
+    sim.step()
+    sim.step()
+    assert sim.acc == 120
+    sim.step()
+    assert sim.acc == 130 
+    sim.step()
 
 def test_store(monkeypatch):
     sim = UVSim()
@@ -149,5 +149,77 @@ def test_store(monkeypatch):
     sim.step()
     assert sim.acc == 5255
     
+
+def test_branches(monkeypatch):
+    # Test unconditional branch (40)
+    sim = UVSim()
+    """
+    0: RDKB #98
+    1: BR #4
+    2: HALT
+    3: HALT
+    4: HALT
+    """
+
+    sim.loadProgram([1098, 4004, 4300, 4300, 4300])
+    monkeypatch.setattr('builtins.input', lambda _: "20")
+    sim.step()
+    sim.step()
+    sim.step()
+    
+    # Tested against 5 because the program counter increments after an instruction is fetched
+    assert sim.pc == 5
+
+    # Test conditional branch on negative (41)
+    sim.reset()
+
+    """
+    0: RDKB #98
+    1: LDA #98
+    2: BRn #4
+    3: RDKB #98
+    4: LDA #98
+    5: BRn #5
+    6: HALT
+    7: HALT
+    """
+    sim.loadProgram([1098, 2098, 4106, 1098, 2098, 4107, 4300, 4300])
+    monkeypatch.setattr('builtins.input', lambda _: "20")
+    sim.step()
+    sim.step()
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "-50")
+    sim.step()
+    sim.step()
+    sim.step()
+    sim.step()
+    
+    assert sim.pc == 8
+
+    # Test conditional branch on zero (41)
+    sim.reset()
+
+    """
+    0: RDKB #98
+    1: LDA #98
+    2: BRn #4
+    3: RDKB #98
+    4: LDA #98
+    5: BRn #5
+    6: HALT
+    7: HALT
+    """
+    sim.loadProgram([1098, 2098, 4206, 1098, 2098, 4207, 4300, 4300])
+    monkeypatch.setattr('builtins.input', lambda _: "1")
+    sim.step()
+    sim.step()
+    sim.step()
+    monkeypatch.setattr('builtins.input', lambda _: "0")
+    sim.step()
+    sim.step()
+    sim.step()
+    sim.step()
+    
+    assert sim.pc == 8
 
 
--- uvsim.py
+++ uvsim.py
@@ -1,86 +1,91 @@
 class UVSim:
-	def __init__(self):
-		self.reset()
+    def __init__(self):
+        self.reset()
 
-	def reset(self):
-		# Create memory with length 100 words
-		self.memory = [0]*100
+        self.debug = True
 
-		# Program counter: track next instruction to execute
-		self.pc = 0
+    def reset(self):
+        # Create memory with length 100 words
+        self.memory = [0]*100
 
-		# Accumulator register
-		self.acc = 0
+        # Program counter: track next instruction to execute
+        self.pc = 0
 
-		# Not running
-		self.running = False
-	
-	def loadProgram(self, data):
-		# Verify we can copy all program data into memory
-		if len(data) > len(self.memory):
-			return False
+        # Accumulator register
+        self.acc = 0
 
-		# Copy all data into memory
-		for i in range(len(data)):
-			self.memory[i] = data[i]
+        # Not running
+        self.running = False
+    
+    def loadProgram(self, data):
+        # Verify we can copy all program data into memory
+        if len(data) > len(self.memory):
+            return False
 
-		return True
+        # Copy all data into memory
+        for i in range(len(data)):
+            self.memory[i] = data[i]
 
-	def run(self):
-		self.running = True
-		while self.running:
-			self.step()
-	def step(self):
-		# Load instruction from memory with program counter
-		instr = self.memory[self.pc]
-		self.pc += 1
+        return True
 
-		# parse instruction (it's in base 10)
-		opcode = abs(instr) // 100
-		operant = instr % 100
-		if instr < 0:
-			opcode = -opcode
+    def run(self):
+        self.running = True
+        while self.running:
+            self.step()
+    def step(self):
+        # Load instruction from memory with program counter
+        instr = self.memory[self.pc]
+        self.pc += 1
 
-		match opcode:
-			case 10:
-				succeeded = False
-				while not succeeded:
-					try:
-						value = int(input(""))
-						if value > 9999 or value < -9999:
-							raise ValueError()
-						succeeded = True
-						self.memory[operant] = value
-					except ValueError:
-						print("Enter a value between -9999 and 9999 (inclusive)")
-				
-			case 11:
-				print(self.memory[operant])
-			case 20:
-				self.acc = self.memory[operant]
-			case 21:
-				self.memory[operant] = self.acc
-			case 30:
-				self.acc += self.memory[operant]
-			case 31:
-				self.acc -= self.memory[operant]
-			case 32:
-				self.acc /= self.memory[operant]
-			case 33:
-				self.acc *= self.memory[operant]
-			case 40:
-				self.pc = operant
-			case 41:
-				if self.acc < 0:
-					self.pc = operant
-			case 42:
-				if self.acc == 0:
-					self.pc = operant
-			case 43:
-				self.running = False
-			case _:
-				print(f"Tried to execute undefined opcode {opcode}. Halting...")
-				self.running = False
-			
+        # parse instruction (it's in base 10)
+        opcode = abs(instr) // 100
+        operant = instr % 100
+        if instr < 0:
+            opcode = -opcode
 
-		#print(f"{self.pc - 1}: acc:{self.acc} opc:{opcode}, operand:{operant}")
+        if self.debug:
+            print(f"{self.pc - 1}: acc:{self.acc} opc:{opcode}, operand:{operant}")
+
+
+        match opcode:
+            case 10:
+                succeeded = False
+                while not succeeded:
+                    try:
+                        value = int(input(""))
+                        if value > 9999 or value < -9999:
+                            raise ValueError()
+                        succeeded = True
+                        self.memory[operant] = value
+                    except ValueError:
+                        print("Enter a value between -9999 and 9999 (inclusive)")
+                
+            case 11:
+                print(self.memory[operant])
+            case 20:
+                self.acc = self.memory[operant]
+            case 21:
+                self.memory[operant] = self.acc
+            case 30:
+                self.acc += self.memory[operant]
+            case 31:
+                self.acc -= self.memory[operant]
+            case 32:
+                self.acc /= self.memory[operant]
+            case 33:
+                self.acc *= self.memory[operant]
+            case 40:
+                self.pc = operant
+            case 41:
+                if self.acc < 0:
+                    self.pc = operant
+            case 42:
+                if self.acc == 0:
+                    self.pc = operant
+            case 43:
+                self.running = False
+            case _:
+                print(f"Tried to execute undefined opcode {opcode}. Halting...")
+                self.running = False
+            
+
