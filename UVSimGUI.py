24361f08c0766c0f01c90e338edeaaf8253cb6e9
frankge777 <157652504+frankge777@users.noreply.github.com>
2024-03-04 18:16:54 -0700

    Update UVSimGUI.py

--- UVSimGUI.py
+++ UVSimGUI.py
@@ -1,20 +1,34 @@
 import tkinter as tk
 from tkinter import filedialog
 
-
 from uvsim import UVSim
 
 class UVSimGUI:
+    """A GUI for the UVSim application."""
+
     def __init__(self, master):
+        """
+        Initialize the UVSimGUI.
+
+        Args:
+            master (tk.Tk): The root window.
+        """
         self.master = master
         self.uv_sim = UVSim()
 
         self.create_widgets()
 
     def write_output(self, line):
+        """
+        Write output to the output text area.
+
+        Args:
+            line (str): The line to be written.
+        """
         self.output_text.insert(tk.END, line)
 
     def create_widgets(self):
+        """Create GUI widgets."""
         # Output display
         self.output_label = tk.Label(self.master, text="Output:")
         self.output_label.pack()
@@ -36,6 +50,7 @@ class UVSimGUI:
         self.button_frame.pack()
 
     def load_program(self):
+        """Load a program from file."""
         filename = filedialog.askopenfilename()
         if filename:
             with open(filename, "r") as file:
@@ -50,6 +65,7 @@ class UVSimGUI:
         self.uv_sim.start()
 
     def enter_input(self):
+        """Enter input for the program."""
         try:
             value = int(self.input_entry.get())
             if value > 9999 or value < -9999:
@@ -59,7 +75,7 @@ class UVSimGUI:
 
 
     def run_program(self):
-        # TODO: tie this segment to submission of the console input, then restart the sim either by enter key or button
+        """Run the loaded program."""
         if self.uv_sim.required_input:
             self.uv_sim.input = self.input_entry.get()
             self.uv_sim.hasInput = True
@@ -73,7 +89,7 @@ class UVSimGUI:
 
 
     def display_output(self):
+        """Display the output of the program."""
         self.output_text.delete(1.0, tk.END)  # Clear previous output
         for line in self.uv_sim.debug_output:
             self.output_text.insert(tk.END, line + "\n")
-
